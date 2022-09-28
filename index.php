6700dd09b78d41f0be6f7a2dbbc0e5b643d833fc
maralexv <scarvesfamily@yahoo.com>
2022-09-28 20:33:01 +0200

    moved files to repo

---
+++ index.php
@@ -0,0 +1,171 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Binance Instruction</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu&display=swap" rel="stylesheet">
+    <style>
+        body {
+            font-family: 'Ubuntu', sans-serif;
+            font-size: 1.5rem;
+        }
+
+        div {
+            width: 95%;
+            margin: Auto;
+            padding-left: 3em;
+            padding-right: 3em;
+            text-align: center;
+        }
+
+        img {
+            width: 85%;
+        }
+
+        footer {
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+
+    <div>
+        <h1>Как купить крипту с помощю карты Tinkoff, превратить крипту в GBP (или евро) и перечислить деньги на свой счет</h1>
+        <ol>
+            <li>Создаем свой account на
+                <a href="https://binance.com">Binance</a> -
+                крупнейшей криптобирже в мире.<br>
+                Потребуется загран паспорт РФ и комп с камерой. <br>
+                И нужно будет установить приложение Binance на смартфон. <br>
+                Пока Binance не проверит вашу личность, покупать и продавать будет нельзя.<br>
+                <img src='1.png'>
+            </li>
+            <li>
+                Вот тут, в Identification, можно проверить все ли виды операций вам доступны... Или повторить идентификацию, если что-то не сработало.<br>
+                <img style="margin: Auto; width: 35%;" src='2.png'>
+            </li>
+            <li>Все должно выглядеть примерно так, как на картинке ниже. <br>
+                Нам понядобятся P2P Transactions. <br>
+                Так что они должны быть доступны.<br>
+                <img src='3.png'>
+            </li>
+            <li>
+                Когда все будет уже доступно, идем в верхнее меню, нажимаем Trade, а потом P2P. <br>
+                <img src='4.png'>
+            </li>
+            <li>
+                Там ОЧЕНЬ советую просмотреть User Guide. <br>
+                <img src='5.png'>
+            </li>
+            <li>
+                Теперь можно начинать покупать крипту<br>
+                1 - сначала выбираем нужную нам крипту – <strong>USDT</strong>. Она привязана к USD, что сильно снижает риски скачков курса криптовалют.<br>
+                2 - потом выбираем Fiat (обычные деньги), которыми мы собираемся платить за USDT. В нашем случае - RUB (рубли)<br>
+                <img src='6.png'>
+            </li>
+            <li>
+                Чтобы облегчить поиск подходящего контрагента, заводим примерную сумму рублей, которую мы хотим обменять на USDT<br>
+                Кликаем Search.<br>
+                <img src='7.png'>
+            </li>
+            <li>
+                Чтобы найти всех тех, кто принимает платежи в Tinkoff, отфильтровываем Payment по 'Tinkoff'.<br>
+                А потом из списка (отсортированного по курсу, по возрастанию) выбираем контрагента...<br>
+                Смотрим на количество проведенных сделок и % выполнения<br>
+                А выбрав продавца, нажимаем его зеленую кнопку Buy USDT<br>
+                <img src='8.png'>
+            </li>
+            <li>
+                Тут мы заносим точную сумму рублей, которые мы хотим заплатить за USDT. <br>
+                Видим сколько USDT это нам даст.<br>
+                И нажимаем кнопку Buy USDT - мы практически заключаем контракт.<br>
+                <img src='9.png'>
+            </li>
+            <li>
+                Нам сразу сообщают куда надо заплатить наши рубли.<br>
+                Открываем приложение Tinkoff (или заходим в свой Tinkoff account на компе)<br>
+                И перечисляем обещанную сумму рублей на указанный номер карты Tinkoff<br>
+                Убеждаемся, что деньги ушли.<br>
+                НЕ ЗАБЫВАЕМ СРАЗУ ПОСЛЕ ЭТОГО НАЖАТЬ НА КНОПКУ 'Transferred, notify seller'<br>
+                На все это у нас 15 минут!<br>
+                <img src='10.png'>
+            </li>
+            <li>
+                Крипта обычно поступает очень быстро.<br>
+                Чтобы ее увитдеть, надо зайти в свой Funding Wallet<br>
+                <img src='11.png'>
+            </li>
+            <li>
+                Это должно выглядеть примерно так. Только сумма будет >0.000000000 <br>
+                <img src='12.png'>
+            </li>
+            <li>
+                Теперь мы хотим превратить USDT в GBP. <br>
+                Для этого нам надо переместить (<strong>Transfer</strong>) USDT из 'Funding' Wallet в 'Fiat and Spot' Wallet.<br>
+                <img src='13.png'>
+            </li>
+            <li>
+                Указываем количество (можно нажать MAX) и жмем Confirm.<br>
+                <img src='14.png'>
+            </li>
+            <li>
+                Переходим в 'Fiat and Spot' Wallet.<br>
+                <img src='15.png'>
+            </li>
+            <li>
+                Там находим в списке USDT и жмем Convert, чтобы поменять их на GBP<br>
+                <img src='16.png'>
+            </li>
+            <li>
+                Указываем, сколько USDT будем превращять в GBP. Можно опять нажать MAX. <br>
+                Выбираем валюту (для вас – GBP) и конвертируем.<br>
+                <img src='17.png'>
+            </li>
+            <li>
+                Возвращаемся в Fiat and Spot. Там должны появиться фунты.<br>
+                Теперь их можно <strong>Withdraw</strong>.<br>
+                (обратите внимание на 'Deposit' - так можно переводить Fiat (обычные деньги) со своего банковского счета или с карты в Binance (небольшую сумму). В процессе Binance " запомнит" банковский счет или карту, на который(ую) потом можно будет Withdraw деньги)<br>
+                <img src='18.png'>
+            </li>
+            <li>
+                В новом окошке можно выбрать куда вы будете withdraw деньги...<br>
+                Нажимаем Continue.<br>
+                <img src='19.png'>
+            </li>
+            <li>
+                Теперь вводим сумму GBP (или что у вас там есть) и опять жмем Continue.<br>
+                <img src='20.png'>
+            </li>
+        </ol>
+        <h2>Еще один-два шага после этого (там все просто и понятно) и все! Теперь терпеливо ждем прихода денег.</h2><br>
+        <ul>
+            <li>
+                Если вдруг неуютно и кажется, что что-то не так, то через Wallet можно зайти в Transaction History. <br>
+                И там все посмотреть: <br>
+                <img src='22.png'><br>
+                <img src='23.png'>
+            </li>
+        </ul>
+    </div>
+
+    <div>
+        <h1>УДАЧИ!!!</h1>
+        <footer>
+            Copyright scarvesfamily &copy; <?php echo date("Y"); ?>
+        </footer>
+    </div>
+
+
+</body>
+
+</html>
